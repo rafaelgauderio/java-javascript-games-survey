c3a713a8ee2d529365d2b8801c05993603195fec
Rafael de Luca <deluca1712@gmail.com>
2024-03-18 17:01:03 -0300

    create project

---
+++ backend/src/main/java/com/rafaeldeluca/gamessurver/GamessurveyApplication.java
@@ -0,0 +1,13 @@
+package com.rafaeldeluca.gamessurver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamessurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamessurveyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rafaeldeluca/gamessurver/GamessurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaeldeluca.gamessurver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamessurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

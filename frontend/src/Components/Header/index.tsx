53fac2ec45faae286b5c9f2cc55f54dd532a6097
Rafael de Luca <deluca1712@gmail.com>
2024-03-25 17:05:10 -0300

    Header component stylized and fixing sgv module

--- frontend/src/Components/Header/index.tsx
+++ frontend/src/Components/Header/index.tsx
@@ -1,14 +1,18 @@
-type Params = {
-    message: string;
+import React from 'react';
+import './styles.css';
 
-}
-function Header({ message }: Params) {
+import {ReactComponent as Logo} from '../../images/logo.svg';
+
+function Header() {
 
     return (
-
-        <h2 className="header">
-            <p>Games Survey</p>
-        </h2>
+        <header className="main-header">            
+           <Logo></Logo>
+           <div className="logo-text">
+            <span className="logo-text-first">Site Game</span>
+            <span className="logo-text-second"> Survey</span>
+           </div>
+        </header>
     );
 }
 

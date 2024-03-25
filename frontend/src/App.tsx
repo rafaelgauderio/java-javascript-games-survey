e3a7da8868490a078ddcf0412f4e5702ce5efdea
Rafael de Luca <deluca1712@gmail.com>
2024-03-25 15:04:20 -0300

    cleaning project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Início front-web projeto games survey
-        </p>      
-      </header>
-    </div>
+
+    <h2 className="header">
+      Início front-web projeto games survey
+    </h2>
+
   );
 }
 

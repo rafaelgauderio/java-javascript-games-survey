6ae8d669ee2c8d72ed741533c3dceed4bd5bf9fe
Rafael de Luca <deluca1712@gmail.com>
2024-03-25 15:41:17 -0300

    global css

--- frontend/src/Components/Header/index.tsx
+++ frontend/src/Components/Header/index.tsx
@@ -7,8 +7,7 @@ function Header({ message }: Params) {
     return (
 
         <h2 className="header">
-            <h3>Bem vindo {message} ao início do projeto</h3>
-            <p>Início front-web projeto games survey</p>
+            <p>Games Survey</p>
         </h2>
     );
 }

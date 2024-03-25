559b821d61c39643ca9407927f8c1878152324d9
Rafael de Luca <deluca1712@gmail.com>
2024-03-25 15:22:09 -0300

    Passing parameters as Props

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,10 @@
-import React from 'react';
-
 import './App.css';
 import Header from './Components/Header';
 
 function App() {
   return (
     <div>
-      <Header />
-      <Header />
+      <Header message={"Rafael de Luca"} />
     </div>
   );
 }
--- frontend/src/Components/Header/index.tsx
+++ frontend/src/Components/Header/index.tsx
@@ -1,12 +1,16 @@
-function Header() {
+type Params = {
+    message: string;
+
+}
+function Header({ message }: Params) {
 
     return (
 
         <h2 className="header">
-            Início front-web projeto games survey
+            <h3>Bem vindo {message} ao início do projeto</h3>
+            <p>Início front-web projeto games survey</p>S
         </h2>
     );
-
 }
 
 export default Header;

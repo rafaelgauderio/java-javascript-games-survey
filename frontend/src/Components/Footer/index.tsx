8b3d0293a7fc877e08f5d7c5cda94f52e1c41932
Rafael de Luca <deluca1712@gmail.com>
2024-03-26 15:16:19 -0300

    fixing footer css styling

--- frontend/src/Components/Footer/index.tsx
+++ frontend/src/Components/Footer/index.tsx
@@ -4,9 +4,9 @@ function Footer() {
 
     return (
         <div id="#wrapper">
-            <div className="logo-text footer">
-                <span className="logo-text-second">Desenvolvido por Rafael de Luca</span>
-                <span className="logo-text-second">Todos os direitos reservados</span>
+            <div className="footer">
+                <span className="logo-text logo-text-second">Desenvolvido por <text className="footer-text">Rafael de Luca</text></span>
+                <span className="logo-text logo-text-second">Todos os direitos reservados</span>
             </div>
         </div>
     );

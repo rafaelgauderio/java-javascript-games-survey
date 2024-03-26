8e831b2a02e6a02c4d29141897147906ace951ac
Rafael de Luca <deluca1712@gmail.com>
2024-03-26 17:22:39 -0300

    page Component Home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Footer />
-    </div>
+    <>
+      <div>
+        <Header />
+        <Home />
+        <Footer />
+      </div>
+    </>
   );
 }
 

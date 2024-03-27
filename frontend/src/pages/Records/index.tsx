b311d0ddde3d57be2d92a9ee41cb8dd439064eb5
Rafael de Luca <deluca1712@gmail.com>
2024-03-27 15:37:06 -0300

    Table Records with mock data

--- frontend/src/pages/Records/index.tsx
+++ frontend/src/pages/Records/index.tsx
@@ -2,7 +2,84 @@ import "./styles.css";
 
 function Records() {
     return (
-        <h2> Records </h2>
+        <div className="page-container">
+            <table className="records-table" cellSpacing={"0"} cellPadding={"5"}>
+                <thead>
+                    <th>DATA-HORA</th>
+                    <th>NOME</th>
+                    <th>IDADE</th>
+                    <th>PLATAFORMA</th>
+                    <th>GÊNERO</th>
+                    <th>NOME DO JOGO</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>23/12/2023</td>
+                        <td>Rafael de Luca</td>
+                        <td>35</td>
+                        <td>PLAYSTATION</td>
+                        <td>Ação - Aventura</td>
+                        <td>Super Mario Brothers</td>
+                    </tr>
+                    <tr>
+                        <td>23/12/2023</td>
+                        <td>Rafael de Luca</td>
+                        <td>35</td>
+                        <td>PLAYSTATION</td>
+                        <td>Ação - Aventura</td>
+                        <td>Super Mario Brothers</td>
+                    </tr>
+                    <tr>
+                        <td>23/12/2023</td>
+                        <td>Rafael de Luca</td>
+                        <td>35</td>
+                        <td>PLAYSTATION</td>
+                        <td>Ação - Aventura</td>
+                        <td>Super Mario Brothers</td>
+                    </tr>
+                    <tr>
+                        <td>23/12/2023</td>
+                        <td>Rafael de Luca</td>
+                        <td>35</td>
+                        <td>PLAYSTATION</td>
+                        <td>Ação - Aventura</td>
+                        <td>Super Mario Brothers</td>
+                    </tr>
+                    <tr>
+                        <td>23/12/2023</td>
+                        <td>Rafael de Luca</td>
+                        <td>35</td>
+                        <td>PLAYSTATION</td>
+                        <td>Ação - Aventura</td>
+                        <td>Super Mario Brothers</td>
+                    </tr>
+                    <tr>
+                        <td>23/12/2023</td>
+                        <td>Rafael de Luca</td>
+                        <td>35</td>
+                        <td>PLAYSTATION</td>
+                        <td>Ação - Aventura</td>
+                        <td>Super Mario Brothers</td>
+                    </tr>
+                    <tr>
+                        <td>23/12/2023</td>
+                        <td>Rafael de Luca</td>
+                        <td>35</td>
+                        <td>PLAYSTATION</td>
+                        <td>Ação - Aventura</td>
+                        <td>Super Mario Brothers</td>
+                    </tr>
+                    <tr>
+                        <td>23/12/2023</td>
+                        <td>Rafael de Luca</td>
+                        <td>35</td>
+                        <td>PLAYSTATION</td>
+                        <td>Ação - Aventura</td>
+                        <td>Super Mario Brothers</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     )
 }
 

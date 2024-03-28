704665b23bb963e5b941f2d95c5d70c9e710a63f
Rafael de Luca <deluca1712@gmail.com>
2024-03-28 15:36:15 -0300

    fixing footer css and lines per page parameter

--- frontend/src/Components/Footer/index.tsx
+++ frontend/src/Components/Footer/index.tsx
@@ -3,7 +3,9 @@ import "./styles.css";
 function Footer() {
 
     return (
-        <div id="#wrapper">
+        <div id="wrapper">
+            <div className="body-content">                
+            </div>
             <div className="footer">
                 <span className="logo-text logo-text-second">Desenvolvido por <p className="footer-text">Rafael de Luca</p></span>
                 <span className="logo-text logo-text-second">Todos os direitos reservados</span>

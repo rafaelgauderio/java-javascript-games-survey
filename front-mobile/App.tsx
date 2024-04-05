1e63c65bb7984240c4a7dc0ac3dce0d7db169771
Rafael de Luca <deluca1712@gmail.com>
2024-04-05 12:36:21 -0300

    adding style css react native

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Projeto com game survey versão mobile!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Projeto game survey versão mobile com react native!</Text>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +13,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'orange',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    color: 'white', 
+    
   },
+  text: {
+    display: 'flex',
+    color: 'white',
+    fontSize: 35,
+    fontFamily: 'Arial Black',
+    justifyContent: "center",
+    alignItems: 'center',
+    textShadowColor:'#989898',
+    textShadowOffset:{width: 5, height: 5},
+    textShadowRadius:10,
+    borderWidth: 4,
+    borderColor: "thistle",
+    borderRadius: 20,
+    marginHorizontal: 10, 
+    marginVertical: 20,
+    padding: 20,    
+
+  }
 });
